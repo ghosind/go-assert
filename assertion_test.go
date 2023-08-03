d69c25ae15452d8e482964c8bf4bc1570517c855
ghosind <ghosind@gmail.com>
2023-08-03 20:27:06 +0800

    test: remove unused test cases.

--- assertion_test.go
+++ assertion_test.go
@@ -1,7 +1,6 @@
 package assert
 
 import (
-	"os"
 	"testing"
 )
 
@@ -12,43 +11,5 @@ func TestNewAssertion(t *testing.T) {
 
 	NotPanic(t, func() {
 		New(new(testing.T))
-	})
-}
-
-func TestFailed(t *testing.T) {
-	testFailed(t, func(a *Assertion) {
-		// do nothing
-	}, false)
-	testFailed(t, func(a *Assertion) {
-		a.Log("test")
-	}, false)
-	testFailed(t, func(a *Assertion) {
-		a.Logf("test")
-	}, false)
-	testFailed(t, func(a *Assertion) {
-		a.Error("test")
-	}, true)
-	testFailed(t, func(a *Assertion) {
-		a.Errorf("test")
-	}, true)
-	testFailed(t, func(a *Assertion) {
-		a.Fail()
-	}, true)
-}
-
-func testFailed(t *testing.T, f func(a *Assertion), failed bool) {
-	assert := New(new(testing.T))
-
-	f(assert)
-	DeepEqual(t, assert.Failed(), failed)
-}
-
-func TestRun(t *testing.T) {
-	assert := New(t)
-
-	assert.Setenv("env", "assert test")
-	assert.Run("test Run", func(t *testing.T) {
-		DeepEqual(t, t.Name(), "TestRun/test_Run")
-		DeepEqual(t, os.Getenv("env"), "assert test")
 	})
 }
